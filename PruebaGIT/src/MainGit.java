03b806e95045443f300563e194e2d1c1b87d172c
Eduardo Jorge Sanjurjo Aramburu <eduardo.sanjurjo@opendeusto.es>
2022-10-04 13:19:51 +0200

    Primer Commit de proyecto de prueba.

---
+++ PruebaGIT/src/MainGit.java
@@ -0,0 +1,15 @@
+
+public class MainGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Repositorio Local(Eclipse) vs Repositorio Remoto(Git->Obligación de sincronizar)
+		//file>Teams>nuevo directorio según proyecto
+		//Configurar nombre(git > configuration) + Mensaje > COMMIT
+		
+		
+		
+		System.out.println("Hola mundo!");
+	}
+
+}
